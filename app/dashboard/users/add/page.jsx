4f847302f1fb6d3fee7fdbb5766ee648e30a1839
ahihi <bdsthocu20@gmail.com>
2023-12-06 15:26:32 +0700

    #19

--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -1,10 +1,11 @@
 
 import styles from "@/app/ui/dashboard/users/addUser/addUser.module.css";
+import { addUser } from "@/app/lib/actions";
 
 const AddUserPage = () => {
     return (
         <div className={styles.container}>
-            <form className={styles.form}>
+            <form action={addUser} className={styles.form}>
                 <input type="text" placeholder="username" name="username" required />
                 <input type="email" placeholder="email" name="email" required />
                 <input
